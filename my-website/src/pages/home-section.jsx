a26768ad538ca77df41dba622c5e345aec5023aa
Albert De Villa <devillalbert@gmail.com>
2025-09-26 16:42:54 +0800

    updated

--- my-website/src/pages/home-section.jsx
+++ my-website/src/pages/home-section.jsx
@@ -4,15 +4,39 @@ import "../styles/home-section.css";
 
 function HomeSection({ marginTop }) {
   return (
-    <section id="home" className="home-container"
+    <section id="home" className="home-section"
       style={{ marginTop: `${marginTop}px` }}>
 
       <div
         className="info-container"
 
       >
-        <div className="first-container all-container">1st container</div>
-        <div className="second-container all-container">2nd container</div>
+        <div className="first-container ">1st container</div>
+        <div className="second-container ">
+          <div className="first-stat-div">
+            <div className="stat-box lg">Heart Rate</div>
+            <div className="stat-box lg">Activity</div>
+            <div className="stat-box lg">Anomally</div>
+            <div className="stat-box lg">Device Battery</div>
+            <div className="stat-box bold">78bpm</div>
+            <div className="stat-box bold">Stationary</div>
+            <div className="stat-box bold">
+              <p className="rg">Warning</p></div>
+            <div className="stat-box bold">85%</div>
+          </div>
+
+          <div className="second-stat-div">
+            <div className="stat-box lg">Heart Rate</div>
+            <div className="stat-box lg">Activity</div>
+            <div className="stat-box lg">Anomally</div>
+            <div className="stat-box lg">Device Battery</div>
+            <div className="stat-box bold">78bpm</div>
+            <div className="stat-box bold">Stationary</div>
+            <div className="stat-box bold">warning</div>
+            <div className="stat-box bold">85%</div>
+          </div>
+          <div className="graph-div">1st</div>
+        </div>
 
       </div>
     </section>
