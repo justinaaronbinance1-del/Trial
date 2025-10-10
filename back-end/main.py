22f1e452d141565cb20a3e39911772d33af179c2
Albert De Villa <devillalbert@gmail.com>
2025-10-10 08:41:35 +0800

    removed front-end folder

--- back-end/main.py
+++ back-end/main.py
@@ -1,33 +1,37 @@
 from fastapi import FastAPI, Request
 from datetime import datetime
 
+
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {"Message": "✅ IOT Dashboard Backend is running!"}
+    return {"Message": "IOT Dashboard Backend is running!"}
 
 
-# ESP32 sends its data here
 @app.post("/data")
 async def receive_data(request: Request):
-    # Extract data from the POST request (supports form or JSON)
+    
     form_data = await request.form()
     heart_rate = float(form_data.get("heart_rate", 0))
-    motion = float(form_data.get("motion", 0))
+    motion_num = int(form_data.get("motion", 0))
+    timestamp = datetime.now()  
+
+    motion_dict = {0: "Standby", 1: "Running", 2: "Moving"}
+    motion_str = motion_dict.get(motion_num, "Unknown")
 
     
-    print("===================================")
-    print(f"ESP32 Connected at: {datetime.now()}")
+    print(f"===================================")
+    print(f"ESP32 Connected at: {timestamp}")
     print(f" Heart Rate: {heart_rate}")
-    print(f" Motion: {motion}")
-    print("===================================\n")
+    print(f" Motion: {motion_str}")
+    print(f"===================================\n")
 
     
     return {
-        "status": "✅ received successfully",
+        "status": " received successfully",
         "heart_rate": heart_rate,
-        "motion": motion,
-        "timestamp": datetime.now().isoformat()
+        "motion": motion_str,
+        "timestamp": timestamp.isoformat()
     }
