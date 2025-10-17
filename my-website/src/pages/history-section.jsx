81a2a07e22b6968766ba4d900aaa3380de9de993
danxien <0322-3637@lspu.edu.ph>
2025-10-17 18:51:05 +0800

    Roldan

---
+++ my-website/src/pages/history-section.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect }  from "react";
+
+function HistorySection(){
+    return (
+      <section id="history" className="history-info-container">
+        History content here...
+      </section>
+    )
+
+}
+
+export default HistorySection;
