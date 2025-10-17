dde9808f53396486961b280400825b36fff019ed
danxien <0322-3637@lspu.edu.ph>
2025-10-17 22:26:29 +0800

    Updated website files and added new sections

---
+++ my-website/src/pages/about-section.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect }  from "react";
+
+function AboutSection(){
+    return (
+      <section id="about-us" className="about-us-info-container">
+        About Us content here...
+      </section>
+    )
+
+}
+
+export default AboutSection;
