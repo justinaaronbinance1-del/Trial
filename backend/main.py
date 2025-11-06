469038e6997eac5487f54f411a99288e708198dd
Albert De Villa <devillalbert@gmail.com>
2025-11-06 15:07:37 +0800

    Backend part

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,84 @@ import mysql.connector
 import joblib
 import pandas as pd
 
+
+
 rf_model = joblib.load("rf_motion_model.pkl")
 scaler = joblib.load("scaler.pkl")
-
+heartrate_avg = 0
+min_heartrate = 0
+max_heartrate = 0
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"IOT Dashboard Backend is running!"}
-  
+    return {"message": "IOT Dashboard Backend is running!"}
+
+
+@app.post("/data")
+async def receive_data(request: Request):
+    
+    form_data = await request.form()
+    data = load_sensor_data(form_data)
+    
+    try: 
+            connection = get_connection()
+            cursor = connection.cursor()
+
+            state = predict_activity(
+                data["ax"], data["ay"], data["az"],
+                data["gx"], data["gy"], data["gz"]
+            )
+
+            heartRate_stat, spo2_stat = validate_vital_signs(data["heartRate"], data["spo2"])
+            avg_heartrate, max_heartrate, min_heartrate = summary_compute(cursor)
+            stud_condition = predict_condition(state, heartRate_stat)
+
+            print_sensor_log(
+                data["timestamp"], data["ax"], data["ay"], data["az"],
+                data["gx"], data["gy"], data["gz"],
+                state, heartRate_stat, spo2_stat, stud_condition, avg_heartrate,
+                max_heartrate, min_heartrate
+            )
+
+
+            sql = """ 
+                INSERT INTO heart_rate_motion_readings (recorded_at, ax, ay, az, gx, gy, gz, heart_rate, spo2, predicted_activity, stud_condition)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            values = (
+                data["timestamp"], data["ax"],
+                data["ay"], data["az"], data["gx"],
+                data["gy"], data["gz"], heartRate_stat, spo2_stat, state, stud_condition
+                
+            )
+
+            cursor.execute(sql, values)
+            connection.commit()
+            print("✅ Data successfully saved to heart_rate_motion_readings.")
+
+    except mysql.connector.Error as e:
+            print("❌ Database error:", e)
+
+    finally: 
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+            return {
+                "status": "Data received successfully ✅",
+                "timestamp": data["timestamp"],
+                "acceleration": {"x": data["ax"], "y": data["ay"], "z": data["az"]},
+                "gyroscope": {"x": data["gx"], "y": data["gy"], "z": data["gz"]},
+                "heartrate": heartRate_stat,
+                "oxygen_saturation": spo2_stat,
+                "stud_condition": stud_condition,
+                "AVG Heart Rate": avg_heartrate,
+                "Max Heart Rate": max_heartrate,
+                "Min Heart Rate": min_heartrate
+            }  
 
 def load_sensor_data(form_data):
     return{
@@ -26,7 +94,6 @@ def load_sensor_data(form_data):
         "gz" : float(form_data.get("gz", 0)),
         "heartRate" : int(form_data.get("heartRate", 0)),
         "spo2" : int(form_data.get("spo2", 0)),
-        "wavePulse" : int(form_data.get("wavePulse", 0)),
         "timestamp" : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
 
@@ -41,16 +108,42 @@ def predict_activity(ax, ay, az, gx, gy, gz):
        return "Stationary"
    else: 
        return "Walking"
+
+def predict_condition(state, heartRate_stat):
+     if heartRate_stat == "⚠️ No valid heart rate!":
+        return  "No valid heart rate!"
+     elif state == "Walking":
+          if 190 < heartRate_stat:
+             return "High Heart Rate while Walking!"
+          else:
+               return "Normal"
+     elif state == "Stationary":
+          if 160 < heartRate_stat:
+               return "High Heart Rate while not Moving!"
+          else: 
+               return "Normal"
+     elif state == "Running":
+          if 210 < heartRate_stat:
+               return "High Heart Rate While Running!"
+          return "Normal"
+     else: 
+          return "Normal"
    
-
-
 def validate_vital_signs(heartRate, spo2):
-    heartRate_stat = heartRate if heartRate not in [999, -999] else "⚠️ No valid heart rate!"
-    spo2_stat = spo2 if spo2 not in [999, -999] else "⚠️ No valid SpO₂!"
+    heartRate_stat = heartRate if heartRate not in [999, -999, 0] else "⚠️ No valid heart rate!"
+    spo2_stat = spo2 if spo2 not in [999, -999, 0] else "⚠️ No valid SpO₂!"
     return heartRate_stat, spo2_stat
+    
+def summary_compute(cursor):
+       cursor.execute("""
+    SELECT AVG(heart_rate), MAX(heart_rate), MIN(heart_rate) FROM heart_rate_motion_readings
+    """) 
+       avg_heartrate, max_heartrate, min_heartrate = cursor.fetchone()
+       return avg_heartrate, max_heartrate, min_heartrate
 
      
-def print_sensor_log(timestamp, ax, ay, az, gx, gy, gz, state, heartRate_stat, spo2_stat, wavePulse):
+def print_sensor_log(timestamp, ax, ay, az, gx, gy, gz, state, heartRate_stat, spo2_stat, stud_condition, avg_heartrate, max_heartrate,
+                     min_heartrate):
    
     print("===================================")
     print(f"📡 Data received at: {timestamp}")
@@ -59,65 +152,10 @@ def print_sensor_log(timestamp, ax, ay, az, gx, gy, gz, state, heartRate_stat, s
     print(f"  ➤ Activity:      {state}")
     print(f"  ➤ Heart Rate:    {heartRate_stat}")
     print(f"  ➤ SpO₂:          {spo2_stat}")
-    print(f"  ➤ Wave Pulse:    {wavePulse}")("===================================\n")    
+    print(f"  ➤ Student Condition:          {stud_condition}")
+    print(f"  ➤ AVG Heart Rate:          {int(avg_heartrate)}")
+    print(f"  ➤ Max Heart Rate:          {max_heartrate}")
+    print(f"  ➤ Min Heart Rate:          {min_heartrate}")
+    print("===================================\n")    
 
 
-@app.post("/data")
-async def receive_data(request: Request):
-    
-    form_data = await request.form()
-    data = load_sensor_data(form_data)
-    
-    state = predict_activity(
-        data["ax"], data["ay"], data["az"],
-        data["gx"], data["gy"], data["gz"]
-    )
-
-    heartRate_stat, spo2_stat = validate_vital_signs(data["heartRate"], data["spo2"])
-
-
-
-    print_sensor_log(
-        data["timestamp"], data["ax"], data["ay"], data["az"],
-        data["gx"], data["gy"], data["gz"],
-        state, heartRate_stat, spo2_stat, data["wavePulse"]
-    )
-    
-
-    try: 
-            connection = get_connection()
-            cursor = connection.cursor()
-
-            sql = """ 
-                INSERT INTO heart_rate_motion_readings (recorded_at, ax, ay, az, gx, gy, gz, heart_rate, spo2, predicted_activity, stud_condition)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """
-
-            values = (
-                data["timestamp"], data["ax"],
-                data["ay"], data["az"], data["gx"],
-                data["gy"], data["gz"], heartRate_stat, spo2_stat, state, None
-                
-            )
-            cursor.execute(sql, values)
-            connection.commit()
-            print("✅ Data successfully saved to heart_rate_motion_readings.")
-
-    except mysql.connector.Error as e:
-            print("❌ Database error:", e)
-
-    finally: 
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-
-
-            return {
-                "status": "Data received successfully ✅",
-                "timestamp": data["timestamp"],
-                "acceleration": {"x": data["ax"], "y": data["ay"], "z": data["az"]},
-                "gyroscope": {"x": data["gx"], "y": data["gy"], "z": data["gz"]},
-                "heartrate": heartRate_stat,
-                "oxygen_saturation": spo2_stat,
-                "waveform_value": data["wavePulse"]
-            }
